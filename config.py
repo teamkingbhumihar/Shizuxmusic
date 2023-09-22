c9677d30b8104bd8053a35479bef588b4c918a0e
teamkingbhumihar <133948972+teamkingbhumihar@users.noreply.github.com>
2023-09-22 18:25:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝔨𝔦𝔫𝔤 𝔵 𝔪𝔲𝔰𝔦𝔠")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "missbhumihar 𝔪𝔲𝔰𝔦𝔠")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6298886738").split()))
 
